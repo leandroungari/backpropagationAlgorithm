19ec5af4eed8b75d1eabbe708ac48cf5d0f47770
Leandro Ungari <leandroungari@gmail.com>
2017-07-07 08:30:31 -0300

    Classe Backpropagation

---
+++ src/algorithm/BackpropagationAlgorithm.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+/**
+ *
+ * @author leandroungari
+ */
+public class BackpropagationAlgorithm {
+    
+    private int[] entrada;
+    private int[] saida;
+    
+    private int[][] pesos;
+    
+    public BackpropagationAlgorithm(int[] entrada){
+        this.entrada = entrada;
+    }
+    
+    public void inicializarPesos(){
+        
+        
+    }
+}
